5a586d53781397940bb9da161d1fbf906b1a6466
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-19 01:35:32 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,52 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
-// =====================
-// Waveshare ESP32-C6 LCD 1.47
-// =====================
+#define GFX_BL 15
 
-// Backlight
-#define TFT_BL 22
-
-// SPI Bus
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
-    15, // DC
-    14, // CS
-    7,  // SCK
-    6,  // MOSI
-    -1  // MISO
+    6 /* DC */,
+    7 /* CS */,
+    10 /* SCK */,
+    11 /* MOSI */,
+    -1 /* MISO */
 );
 
-// ST7789 Display
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
-    21,   // RST
-    1,    // Rotation
-    true, // IPS
-    172,  // Width
-    320,  // Height
-    34,   // Column Offset 1
-    0,    // Row Offset 1
-    34,   // Column Offset 2
-    0     // Row Offset 2
+    14 /* RST */,
+    0 /* rotation */,
+    true /* IPS */,
+    172,
+    320,
+    34,
+    0,
+    34,
+    0
 );
 
 void setup()
 {
-    // Serial Debug
+    pinMode(GFX_BL, OUTPUT);
+    digitalWrite(GFX_BL, HIGH);
+
     Serial.begin(115200);
-    Serial.println("Starting LCD...");
 
-    // Backlight ON
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
-
-    delay(200);
-
-    // Init Display
     gfx->begin();
 
-    // Fill Black
-    gfx->fillScreen(0x0000);
+    gfx->fillScreen(BLACK);
 
-    // White Text
-    gfx->setTextColor(0xFFFF);
+    gfx->setTextColor(WHITE);
     gfx->setTextSize(2);
 
-    // Title
     gfx->setCursor(20, 40);
     gfx->println("WAVESHARE");
 
-    gfx->setCursor(20, 70);
+    gfx->setCursor(20, 80);
     gfx->println("ESP32-C6");
 
-    // Green Box
-    gfx->fillRect(20, 130, 130, 40, 0x07E0);
-
-    // Red Box
-    gfx->fillRect(20, 190, 130, 40, 0xF800);
-
-    // Blue Box
-    gfx->fillRect(20, 250, 130, 40, 0x001F);
-
-    Serial.println("LCD OK");
+    gfx->fillRect(20, 140, 120, 40, RED);
+    gfx->fillRect(20, 200, 120, 40, GREEN);
+    gfx->fillRect(20, 260, 120, 40, BLUE);
 }
 
 void loop()
