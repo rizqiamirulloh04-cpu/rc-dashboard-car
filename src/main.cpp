0b3847a40462d7ce895885c64595ad8c53e20851
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-18 23:47:20 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
-#define BLACK   0x0000
-#define WHITE   0xFFFF
-#define RED     0xF800
-#define GREEN   0x07E0
+#define TFT_BL 15
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
-    8,   // DC
-    14,  // CS
-    7,   // SCK
-    6,   // MOSI
+    6,   // DC
+    7,   // CS
+    5,   // SCK
+    4,   // MOSI
     -1   // MISO
 );
 
@@ -20,29 +17,32 @@ Arduino_GFX *gfx = new Arduino_ST7789(
     0,   // rotation
     true,
     172,
-    320
+    320,
+    34,
+    0,
+    0,
+    0
 );
 
 void setup()
 {
-    pinMode(22, OUTPUT);
-    digitalWrite(22, HIGH);
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
 
     gfx->begin();
     gfx->fillScreen(BLACK);
 
-    gfx->setTextColor(RED);
+    gfx->setTextColor(WHITE);
     gfx->setTextSize(3);
     gfx->setCursor(20, 40);
-    gfx->println("RC DASH");
-
-    gfx->setTextColor(WHITE);
-    gfx->setTextSize(2);
-    gfx->setCursor(20, 100);
     gfx->println("ESP32-C6");
 
-    gfx->drawRect(20, 150, 130, 20, WHITE);
-    gfx->fillRect(20, 150, 90, 20, GREEN);
+    gfx->setTextColor(GREEN);
+    gfx->setTextSize(2);
+    gfx->setCursor(20, 100);
+    gfx->println("LCD OK");
+
+    gfx->fillRect(20, 150, 200, 30, RED);
 }
 
 void loop()
