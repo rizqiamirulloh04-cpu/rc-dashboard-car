86c6faf159d5e9813c47c91a5fa9d080f9b83cfe
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-19 00:17:38 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
-#define TFT_BL 15
+#define BLACK   0x0000
+#define WHITE   0xFFFF
+#define RED     0xF800
+#define GREEN   0x07E0
+
+// =======================
+// Waveshare ESP32-C6 LCD 1.47
+// =======================
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
-    6,   // DC
-    7,   // CS
-    5,   // SCK
-    4,   // MOSI
+    4,   // DC
+    5,   // CS
+    6,   // SCK
+    7,   // MOSI
     -1   // MISO
 );
 
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
-    8,    // RST
+    15,   // RST
     0,    // rotation
     true,
     172,
@@ -22,20 +29,29 @@ Arduino_GFX *gfx = new Arduino_ST7789(
 
 void setup()
 {
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
-
     gfx->begin();
+
     gfx->fillScreen(BLACK);
 
-    gfx->setCursor(20, 40);
+    // Judul
+    gfx->setCursor(40, 30);
     gfx->setTextColor(WHITE);
     gfx->setTextSize(2);
-    gfx->println("ESP32-C6 OK");
+    gfx->println("RC DASHBOARD");
 
-    gfx->setCursor(20, 80);
+    // Status
+    gfx->setCursor(70, 80);
     gfx->setTextColor(GREEN);
-    gfx->println("LCD WORKING");
+    gfx->println("CONNECTED");
+
+    // Bar throttle
+    gfx->drawRect(40, 150, 240, 30, WHITE);
+    gfx->fillRect(42, 152, 180, 26, RED);
+
+    // Steering text
+    gfx->setCursor(100, 220);
+    gfx->setTextColor(WHITE);
+    gfx->println("STEERING");
 }
 
 void loop()
