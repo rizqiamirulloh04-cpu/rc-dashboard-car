512bd4884aeb0fc13819bfb19df128ab301ab5b3
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-20 00:05:34 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,57 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
-class LGFX : public lgfx::LGFX_Device {
-    lgfx::Panel_ST7789 _panel;
-    lgfx::Bus_SPI _bus;
+#define BLACK 0x0000
+#define WHITE 0xFFFF
+#define GREEN 0x07E0
 
-  public:
-    LGFX() {
-        {
-            auto cfg = _bus.config();
+// SPI bus
+Arduino_DataBus *bus = new Arduino_ESP32SPI(
+    15, // DC
+    14, // CS
+    7,  // SCK
+    6,  // MOSI
+    -1  // MISO
+);
 
-            cfg.spi_host = SPI2_HOST;
-            cfg.spi_mode = 0;
-            cfg.freq_write = 40000000;
-            cfg.freq_read = 16000000;
+// Display ST7789
+Arduino_GFX *gfx = new Arduino_ST7789(
+    bus,
+    21,   // RST
+    1,    // rotation
+    true, // IPS
+    172,  // width
+    320   // height
+);
 
-            cfg.pin_sclk = 6;
-            cfg.pin_mosi = 7;
-            cfg.pin_miso = -1;
-            cfg.pin_dc   = 14;
+void setup()
+{
+    pinMode(22, OUTPUT);
+    digitalWrite(22, HIGH);
 
-            _bus.config(cfg);
-            _panel.setBus(&_bus);
-        }
+    gfx->begin();
 
-        {
-            auto cfg = _panel.config();
+    gfx->fillScreen(BLACK);
 
-            cfg.pin_cs           = 15;
-            cfg.pin_rst          = 21;
-            cfg.pin_busy         = -1;
+    gfx->setTextColor(GREEN);
+    gfx->setTextSize(2);
 
-            cfg.panel_width      = 172;
-            cfg.panel_height     = 320;
+    gfx->setCursor(25, 40);
+    gfx->println("RC DASHBOARD");
 
-            cfg.offset_x         = 34;
-            cfg.offset_y         = 0;
+    gfx->setCursor(50, 80);
+    gfx->println("ESP32-C6");
 
-            cfg.offset_rotation  = 0;
-            cfg.dummy_read_pixel = 8;
-            cfg.dummy_read_bits  = 1;
+    // Bar outline
+    gfx->drawRect(20, 140, 280, 30, WHITE);
 
-            cfg.readable         = false;
-            cfg.invert           = true;
-            cfg.rgb_order        = false;
-            cfg.dlen_16bit       = false;
-            cfg.bus_shared       = true;
+    // Fill bar
+    gfx->fillRect(20, 140, 180, 30, GREEN);
 
-            _panel.config(cfg);
-        }
-
-        setPanel(&_panel);
-    }
-};
-
-LGFX tft;
-
-void setup() {
-    tft.init();
-    tft.setRotation(1);
-    tft.fillScreen(TFT_BLACK);
-
-    tft.setTextColor(TFT_GREEN);
-    tft.setTextSize(2);
-    tft.drawString("ESP32-C6 OK", 40, 100);
+    gfx->setCursor(90, 190);
+    gfx->println("Throttle");
 }
 
-void loop() {
+void loop()
+{
 }
