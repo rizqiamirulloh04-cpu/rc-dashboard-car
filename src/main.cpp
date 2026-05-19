03a811d59c7aead570d5ac8da18793eaabe96de3
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-19 18:35:07 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,46 @@
 #include <Arduino.h>
-#include <TFT_eSPI.h>
+#include <Arduino_GFX_Library.h>
 
-TFT_eSPI tft = TFT_eSPI();
+Arduino_DataBus *bus = new Arduino_ESP32SPI(
+    8,   // DC
+    7,   // CS
+    6,   // SCK
+    10,  // MOSI
+    -1
+);
+
+Arduino_GFX *gfx = new Arduino_ST7789(
+    bus,
+    3,
+    1,
+    true,
+    172,
+    320
+);
 
 void setup()
 {
     pinMode(22, OUTPUT);
     digitalWrite(22, HIGH);
 
-    tft.init();
-    tft.setRotation(1);
+    gfx->begin();
 
-    tft.fillScreen(TFT_BLACK);
+    gfx->fillScreen(BLACK);
 
-    tft.setTextColor(TFT_GREEN);
-    tft.setTextSize(2);
+    gfx->setTextColor(GREEN);
+    gfx->setTextSize(2);
 
-    tft.drawString("RC DASHBOARD", 20, 40);
-    tft.drawString("ESP32-C6", 40, 80);
+    gfx->setCursor(30, 40);
+    gfx->println("RC DASHBOARD");
 
-    tft.drawRect(20, 140, 280, 30, TFT_WHITE);
-    tft.fillRect(20, 140, 180, 30, TFT_GREEN);
+    gfx->setCursor(50, 80);
+    gfx->println("ESP32-C6");
 
-    tft.drawString("Throttle", 90, 190);
+    gfx->drawRect(20, 140, 280, 30, WHITE);
+    gfx->fillRect(20, 140, 180, 30, GREEN);
+
+    gfx->setCursor(90, 190);
+    gfx->println("Throttle");
 }
 
 void loop()
