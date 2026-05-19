65347d832bc79c03055b2f4ef56f4914213b7f47
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-19 23:20:14 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,69 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <TFT_eSPI.h>
+#include <LovyanGFX.hpp>
 
-TFT_eSPI tft = TFT_eSPI();
+class LGFX : public lgfx::LGFX_Device {
+    lgfx::Panel_ST7789 _panel;
+    lgfx::Bus_SPI _bus;
 
-#define TFT_BL 15
+  public:
+    LGFX() {
+        {
+            auto cfg = _bus.config();
+
+            cfg.spi_host = SPI2_HOST;
+            cfg.spi_mode = 0;
+            cfg.freq_write = 40000000;
+            cfg.freq_read = 16000000;
+
+            cfg.pin_sclk = 6;
+            cfg.pin_mosi = 7;
+            cfg.pin_miso = -1;
+            cfg.pin_dc   = 14;
+
+            _bus.config(cfg);
+            _panel.setBus(&_bus);
+        }
+
+        {
+            auto cfg = _panel.config();
+
+            cfg.pin_cs           = 15;
+            cfg.pin_rst          = 21;
+            cfg.pin_busy         = -1;
+
+            cfg.panel_width      = 172;
+            cfg.panel_height     = 320;
+
+            cfg.offset_x         = 34;
+            cfg.offset_y         = 0;
+
+            cfg.offset_rotation  = 0;
+            cfg.dummy_read_pixel = 8;
+            cfg.dummy_read_bits  = 1;
+
+            cfg.readable         = false;
+            cfg.invert           = true;
+            cfg.rgb_order        = false;
+            cfg.dlen_16bit       = false;
+            cfg.bus_shared       = true;
+
+            _panel.config(cfg);
+        }
+
+        setPanel(&_panel);
+    }
+};
+
+LGFX tft;
 
 void setup() {
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH);
-
     tft.init();
     tft.setRotation(1);
     tft.fillScreen(TFT_BLACK);
 
     tft.setTextColor(TFT_GREEN);
     tft.setTextSize(2);
-    tft.drawString("ESP32-C6 OK", 20, 40);
+    tft.drawString("ESP32-C6 OK", 40, 100);
 }
 
 void loop() {
