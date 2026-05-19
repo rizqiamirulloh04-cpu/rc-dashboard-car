aa6e28df6cfa422b939e2aaacc20f6035939601a
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-19 23:08:04 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+#define TFT_BL 15
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("RC Dashboard Boot");
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
+
+    tft.init();
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+
+    tft.setTextColor(TFT_GREEN);
+    tft.setTextSize(2);
+    tft.drawString("ESP32-C6 OK", 20, 40);
 }
 
 void loop() {
-    Serial.println("Dashboard Running");
-    delay(1000);
 }
